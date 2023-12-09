098b264f7bd78c41dc8344a5f0a4970d19854d67
Vladislav Kuskov <vqskov@gmail.com>
2023-12-09 23:49:21 +0300

    Задача 1

---
+++ homeTask1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+void ShowTheNumbers (int m, int n) {
+    if (n < m) return;
+    Console.Write(m + " ");
+    ShowTheNumbers(m + 1, n);
+}
+
+Console.Write("Введите минимальное значение: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ShowTheNumbers(m, n);
